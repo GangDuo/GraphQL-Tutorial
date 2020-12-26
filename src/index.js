6f9e47b3fec82f583be2564ed7f1dd3cd11c9b67
Onoda <onoda-y-xyz@mve.biglobe.ne.jp>
2020-12-26 15:41:46 +0900

    リレーションシップ忘れ

--- src/index.js
+++ src/index.js
@@ -33,10 +33,12 @@ let users = {
   1: {
     id: '1',
     username: 'Robin Wieruch',
+    messageIds: [1],
   },
   2: {
     id: '2',
     username: 'Dave Davids',
+    messageIds: [2],
   },
 };
 
